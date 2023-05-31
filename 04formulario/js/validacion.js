494576150f9f5805bcd056d2a79082df60c694a4
Alan Gabriel Barrágan Herrera <ag.barragan.h@edu.utc.mx>
2023-05-31 13:52:59 -0600

    ajustes

--- 04formulario/js/validacion.js
+++ 04formulario/js/validacion.js
@@ -1,9 +1,70 @@
-// JS es multiparadigmico y vamos a iniciar a la programacion por funciones.
+// JS es multiparadigmático y vamos a iniciar con programación por funciones.
 
-function validar(formulario){
-    // Hay varias formas para poder obtener los parametros del formulario.
-    if(formulario.nombre.value.length <= 3){
-        alert("Escriba mas de 3 caracteres en el campo nombre");
+function validar(formulario) {
+    // Hay varias formas de obtener los parámetros del formulario.
+    if (formulario.nombre.value.length <= 3) {
+        alert("Escriba más de 3 caracteres en el campo nombre");
         formulario.nombre.focus();
+        return false;
     }
+
+    var checkOk = "qwertyuiopasdfghjklñzxcvbnm" +
+        "QWERTYUIOPASDFGHJKLÑZXCVBNM ";
+
+    var checkString = formulario.nombre.value;
+
+    var todoEsValido = true;
+
+    for (var i = 0; i < checkString.length; i++) {
+        var ch = checkString.charAt(i);
+        var isLetter = false;
+        for (var j = 0; j < checkOk.length; j++) {
+            if (ch === checkOk.charAt(j)) {
+                isLetter = true;
+                break;
+            }
+        }
+        if (!isLetter) {
+            todoEsValido = false;
+            break;
+        }
+    }
+
+    if (!todoEsValido) {
+        alert("Escriba únicamente letras en el campo nombre");
+        formulario.nombre.focus();
+        return false;
+    }
+
+    checkOk = "1234567890";
+    checkString = formulario.edad.value;
+    todoEsValido = true;
+
+    for (var i = 0; i < checkString.length; i++) {
+        var ch = checkString.charAt(i);
+        var isNumber = false;
+        for (var j = 0; j < checkOk.length; j++) {
+            if (ch === checkOk.charAt(j)) {
+                isNumber = true;
+                break;
+            }
+        }
+        if (!isNumber) {
+            todoEsValido = false;
+            break;
+        }
+    }
+
+    if (!todoEsValido) {
+        alert("Escriba únicamente números en el campo edad");
+        formulario.edad.focus();
+        return false;
+    }
+
+    var txt = formulario.email.value;
+    var emailRegex = /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/;
+
+    alert("El email es " + (emailRegex.test(txt) ? "" : "no ") + "válido");
+
+    return emailRegex.test(txt);
 }
--- 05problemasalgoritmos/javascript/problemas.js
+++ 05problemasalgoritmos/javascript/problemas.js
@@ -1,55 +1,103 @@
 function calcularGanancia() {
-    const capital = parseFloat(document.getElementById("capital").value);
+  const capital = parseFloat(document.getElementById("capital").value);
+  if (capital >= 0 && capital <= 1000000) {
     const ganancia = capital * 0.02;
     document.getElementById("ganancia").textContent = `La ganancia después de un mes será: $${ganancia.toFixed(2)}`;
+  } else {
+    document.getElementById("ganancia").textContent = "Ingrese un valor válido dentro del rango de 0 a 1,000,000.";
   }
-  
-  function calcularComisiones() {
-    const ventas = parseFloat(document.getElementById("ventas").value);
-    const sueldoBase = parseFloat(document.getElementById("sueldo-base").value);
+}
+
+function calcularComisiones() {
+  const ventas = parseFloat(document.getElementById("ventas").value);
+  const sueldoBase = parseFloat(document.getElementById("sueldo-base").value);
+  if (ventas >= 0 && ventas <= 1000000 && sueldoBase >= 0 && sueldoBase <= 1000000) {
     const comisiones = ventas * 0.1;
     const total = sueldoBase + comisiones;
     document.getElementById("comisiones").textContent = `Comisiones: $${comisiones.toFixed(2)}`;
     document.getElementById("total").textContent = `Total a recibir: $${total.toFixed(2)}`;
+  } else {
+    document.getElementById("comisiones").textContent = "Ingrese valores válidos dentro del rango de 0 a 1,000,000.";
+    document.getElementById("total").textContent = "";
   }
-  
-  function calcularDescuento() {
-    const totalCompra = parseFloat(document.getElementById("total-compra").value);
+}
+
+function calcularDescuento() {
+  const totalCompra = parseFloat(document.getElementById("total-compra").value);
+  if (totalCompra >= 0 && totalCompra <= 1000000) {
     const descuento = totalCompra * 0.15;
     const totalPagar = totalCompra - descuento;
     document.getElementById("total-pagar").textContent = `Total a pagar: $${totalPagar.toFixed(2)}`;
+  } else {
+    document.getElementById("total-pagar").textContent = "Ingrese un valor válido dentro del rango de 0 a 1,000,000.";
   }
-  
-  function calcularCalificacionFinal() {
-    const califParcial1 = parseFloat(document.getElementById("calif-parcial1").value);
-    const califParcial2 = parseFloat(document.getElementById("calif-parcial2").value);
-    const califParcial3 = parseFloat(document.getElementById("calif-parcial3").value);
-    const califExamen = parseFloat(document.getElementById("calif-examen").value);
-    const califTrabajo = parseFloat(document.getElementById("calif-trabajo").value);
-  
+}
+
+function calcularCalificacionFinal() {
+  const califParcial1 = parseFloat(document.getElementById("calif-parcial1").value);
+  const califParcial2 = parseFloat(document.getElementById("calif-parcial2").value);
+  const califParcial3 = parseFloat(document.getElementById("calif-parcial3").value);
+  const califExamen = parseFloat(document.getElementById("calif-examen").value);
+  const califTrabajo = parseFloat(document.getElementById("calif-trabajo").value);
+
+  if (
+    califParcial1 >= 0 &&
+    califParcial1 <= 100 &&
+    califParcial2 >= 0 &&
+    califParcial2 <= 100 &&
+    califParcial3 >= 0 &&
+    califParcial3 <= 100 &&
+    califExamen >= 0 &&
+    califExamen <= 100 &&
+    califTrabajo >= 0 &&
+    califTrabajo <= 100
+  ) {
     const promedioParciales = (califParcial1 + califParcial2 + califParcial3) / 3;
-    const califFinal = promedioParciales * 0.55 + califExamen * 0.30 + califTrabajo * 0.15;
-  
+    const califFinal = promedioParciales * 0.55 + califExamen * 0.3 + califTrabajo * 0.15;
+
     document.getElementById("calificacion-final").textContent = `Calificación Final: ${califFinal.toFixed(2)}`;
+  } else {
+    document.getElementById("calificacion-final").textContent = "Ingrese valores válidos dentro del rango de 0 a 100.";
   }
-  
-  function calcularPorcentaje() {
-    const totalEstudiantes = parseFloat(document.getElementById("total-estudiantes").value);
-    const totalHombres = parseFloat(document.getElementById("total-hombres").value);
-  
+}
+
+function calcularPorcentaje() {
+  const totalEstudiantes = parseFloat(document.getElementById("total-estudiantes").value);
+  const totalHombres = parseFloat(document.getElementById("total-hombres").value);
+
+  if (totalEstudiantes >= 0 && totalEstudiantes <= 1000000 && totalHombres >= 0 && totalHombres <= 1000000) {
     const porcentajeHombres = (totalHombres / totalEstudiantes) * 100;
     const porcentajeMujeres = 100 - porcentajeHombres;
-  
+
     document.getElementById("porcentaje-hombres").textContent = `Porcentaje de Hombres: ${porcentajeHombres.toFixed(2)}%`;
     document.getElementById("porcentaje-mujeres").textContent = `Porcentaje de Mujeres: ${porcentajeMujeres.toFixed(2)}%`;
+  } else {
+    document.getElementById("porcentaje-hombres").textContent =
+      "Ingrese valores válidos dentro del rango de 0 a 1,000,000.";
+    document.getElementById("porcentaje-mujeres").textContent = "";
   }
-  
-  function calcularEdad() {
-    const anioNacimiento = parseInt(document.getElementById("anio-nacimiento").value);
-    const fechaActual = new Date();
-    const anioActual = fechaActual.getFullYear();
+}
+
+function calcularEdad() {
+  const anioNacimiento = parseInt(document.getElementById("anio-nacimiento").value);
+  const anioActual = new Date().getFullYear();
+
+  if (anioNacimiento >= 0 && anioNacimiento <= anioActual) {
     const edad = anioActual - anioNacimiento;
-  
     document.getElementById("edad").textContent = `Edad: ${edad} años`;
+  } else {
+    document.getElementById("edad").textContent = "Ingrese un valor válido dentro del rango de años.";
   }
-  
+}
+
+function resetForm() {
+  document.getElementById("problem-form").reset();
+  document.getElementById("ganancia").textContent = "";
+  document.getElementById("comisiones").textContent = "";
+  document.getElementById("total").textContent = "";
+  document.getElementById("total-pagar").textContent = "";
+  document.getElementById("calificacion-final").textContent = "";
+  document.getElementById("porcentaje-hombres").textContent = "";
+  document.getElementById("porcentaje-mujeres").textContent = "";
+  document.getElementById("edad").textContent = "";
+}
