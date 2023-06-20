57fa004c01f829a9b9dfbed3e5fb3413b3dd0c69
Alan Gabriel Barrágan Herrera <ag.barragan.h@edu.utc.mx>
2023-06-20 12:28:59 -0600

    Orientado a objetos

--- 06algoritmia/js/algoritmia.js
+++ 06algoritmia/js/algoritmia.js
@@ -1,78 +1,107 @@
-function invertirPalabras() {
-    var palabras = document.getElementById('p1-input').value;
-    var palabrasArray = palabras.split(' ');
-    var palabrasInvertidas = palabrasArray.reverse().join(' ');
-    document.getElementById('p1-output').textContent = palabrasInvertidas;
-  }
-
-  function Problema2() {
-    var x1 = parseInt(document.getElementById('p2-x1').value);
-    var x2 = parseInt(document.getElementById('p2-x2').value);
-    var x3 = parseInt(document.getElementById('p2-x3').value);
-    var x4 = parseInt(document.getElementById('p2-x4').value);
-    var x5 = parseInt(document.getElementById('p2-x5').value);
-  
-    var y1 = parseInt(document.getElementById('p2-y1').value);
-    var y2 = parseInt(document.getElementById('p2-y2').value);
-    var y3 = parseInt(document.getElementById('p2-y3').value);
-    var y4 = parseInt(document.getElementById('p2-y4').value);
-    var y5 = parseInt(document.getElementById('p2-y5').value);
-  
-    var productoEscalar = x1 * y1 + x2 * y2 + x3 * y3 + x4 * y4 + x5 * y5;
-  
-    document.getElementById('p2-output').textContent = productoEscalar;
-  }
-  
-  
-
+//PROBLEMA 2
 function problema2(){
+  //lo primero que necesitamos es obtener los campos de texto
+  var p2_x1 = document.querySelector('#p2-x1').
+  value;
 
-    var p2_x1 = document.querySelector('#p2-x1').value;
-    var p2_x2 = document.querySelector('#p2-x2').value;
-    var p2_x3 = document.querySelector('#p2-x3').value;
-    var p2_x4 = document.querySelector('#p2-x4').value;
-    var p2_x5 = document.querySelector('#p2-x5').value;
-    var p2_y1 = document.querySelector('#p2-y1').value;
-    var p2_y2 = document.querySelector('#p2-y2').value;
-    var p2_y3 = document.querySelector('#p2-y3').value;
-    var p2_y4 = document.querySelector('#p2-y4').value;
-    var p2_y5 = document.querySelector('#p2-y5').value;
+  var p2_x2 = document.querySelector('#p2-x2').
+  value;
 
-    var v1 = [p2_x1, p2_x2, p2_x3, p2_x4, p2_x5];
-    var v2 = [p2_y1, p2_y2, p2_y3, p2_y4, p2_y5];
+  var p2_x3 = document.querySelector('#p2-x3').
+  value;
 
-v1 = v1.sort(function(a, b){
-    return b-a;
-});
-v2 = v2.sort(function(a, b){
-    return b-a;
-});
+  var p2_x4 = document.querySelector('#p2-x4').
+  value;
 
-var p2_producto = 0;
-
-for(var i = 0; i < v1.length; i++){
-    p2_producto += v1[i]*v2[i];
-}
-
-document.querySelector('#p2-outout').textContent = 'Producto Escalar Minimo : ' + p2_producto;
-
-}
+  var p2_x5 = document.querySelector('#p2-x5').
+  value;
 
 
-function contarCaracteresUnicos() {
-    var palabras = document.getElementById('p3-input').value;
-    var palabrasArray = palabras.split(',');
+  var p2_y1 = document.querySelector('#p2-y1').
+  value;
+
+  var p2_y2 = document.querySelector('#p2-y2').
+  value;
+
+  var p2_y3 = document.querySelector('#p2-y3').
+  value;
+
+  var p2_y4 = document.querySelector('#p2-y4').
+  value;
+
+  var p2_y5 = document.querySelector('#p2-y5').
+  value;
+
+  //construimos el vector
+
+  var v1 = [p2_x1, p2_x2, p2_x3, p2_x4, p2_x5];
+  var v2 = [p2_y1, p2_y2, p2_y3, p2_y4, p2_y5];
+
+  //ordenar los vectores
+
+  v1 = v1.sort(function(a, b){
+      return b-a;
   
-    var palabraMasLarga = '';
-    var maxCaracteresUnicos = 0;
+  });
+
+  v2 = v2.sort(function(a, b){
+      return b-a;
   
-    palabrasArray.forEach(function(palabra) {
-      var caracteresUnicos = new Set(palabra).size;
-      if (caracteresUnicos > maxCaracteresUnicos) {
-        maxCaracteresUnicos = caracteresUnicos;
-        palabraMasLarga = palabra;
-      }
-    });
-  
-    document.getElementById('p3-output').textContent = palabraMasLarga;
+  });
+
+  //vamos a realizar el produto escalar
+
+  var p2_prodcuto = 0;
+
+  for(var i = 0; i < v1.length; i++){
+      p2_prodcuto += v1 [i]*v2[i];
   }
+
+  document.querySelector('#p2-output').textContent
+   = 'Producto Escalar Minimo. ' + p2_prodcuto;
+}
+
+//PROBLEMA 1
+function problema1() {
+var pa = document.getElementById('p1-input').value;
+var parray = pa.split(' ');
+var painvertido = parray.reverse().join(' ');
+document.getElementById('p1-output').textContent = painvertido;
+}
+
+//PROBLEMA 3
+function problema3() {
+  var inputPalabras = document.getElementById('p3-input').value;
+  var palabras = inputPalabras.split(',');
+
+  // Verificar si hay palabras en minúscula o con espacios
+  var hayMinusculas = palabras.some(function(palabra) {
+    return /[a-z]/.test(palabra);
+  });
+
+  if (hayMinusculas || inputPalabras !== inputPalabras.toUpperCase()) {
+    alert("Por favor, ingrese todas las palabras en mayúsculas y sin espacios.");
+    return;
+  }
+
+  var palabraMasLarga = '';
+  var maxCaracteresUnicos = 0;
+
+  palabras.forEach(function(palabra) {
+    var caracteresUnicos = new Set(palabra).size;
+    if (caracteresUnicos > maxCaracteresUnicos) {
+      maxCaracteresUnicos = caracteresUnicos;
+      palabraMasLarga = palabra;
+    }
+  });
+
+  var resultado = 'Palabras ingresadas: ' + palabras.join(', ');
+
+  if (palabraMasLarga !== '') {
+    resultado += '\nPalabra con más caracteres únicos: ' + palabraMasLarga;
+    resultado += ' (' + maxCaracteresUnicos + ' caracteres únicos)';
+  }
+
+  document.getElementById('p3-output').textContent = resultado;
+}
+
